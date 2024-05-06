e497e32006383889984384b41873b0dd137dfa2e
Noa <ainizqare@alu.edu.gva.es>
2024-05-06 14:09:37 +0200

    Update

--- src/main/java/com/mp/javaPaymentSDK/enums/PaymentSolutions.java
+++
@@ -1,9 +0,0 @@
-package com.mp.javaPaymentSDK.enums;
-
-public enum PaymentSolutions {
-    caixapucpuce,
-    creditcards,
-    quix,
-    bizum,
-    paypal
-}
--- src/main/java/com/mp/javaPaymentSDK/utils/TestClass.java
+++
@@ -1,9 +0,0 @@
-package com.mp.javaPaymentSDK.utils;
-
-public class TestClass {
-
-    private int testFun() {
-        return 1;
-    }
-
-}
